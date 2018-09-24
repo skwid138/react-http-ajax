ceb78065784f5297cb35761a0b020407183a3b84
Hunter Rancourt <hunter.rancourt@gmail.com>
2018-09-24 12:37:20 -0500

    updated BrowserRouter base server path

--- src/App.js
+++ src/App.js
@@ -5,10 +5,13 @@ import Blog from './containers/Blog/Blog';
 /* Wrapping the entire app in Browser Router allows the */
 /* wrapped components to access routing features */
 
+//* If the server isn't serving the app from / and it's from /another-dir instead then BrowserRouter needs a basename prop with the correct path *//
+/* ie "/another-dir" */
+
 class App extends Component {
   render() {
     return (
-		<BrowserRouter>
+		<BrowserRouter basename="/">
 			<div className="App">
         		<Blog />
       		</div>
